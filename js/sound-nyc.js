4405426e04ca94fe1a8984de99d55637a1a248de
Joseph Ciurej <ciurej2@illinois.edu>
2014-03-20 21:19:54 -0500

    Added basic functionality for event notification overlays.

--- js/sound-nyc.js
+++ js/sound-nyc.js
@@ -1,3 +1,48 @@
+/**
+ * Populates the map with the given events.
+ *
+ * @param _map The D3 SVG object into which the map contents will be loaded.
+ * @param _projection The D3 projection function used to place coordinates.
+ * @param _events A list of objects of the following form:
+ *  {
+ *      "name": string,
+ *      "date": Date,
+ *      "location": string,
+ *      "coordinates": [ float, float ],        <-- [ longitude, latitude ]
+ *      "sound_radii": [ float, float, float ]  <-- [ +160 dB, 159-120 dB, 120-85 dB ]
+ *  }
+ */
+function populateMap( _map, _projection, _events )
+{
+	var overlay = _map.append( "g" );
+	var projectionFunction = function( _data ) {
+		return "translate(" + _projection( _data["coordinates"] ) + ")";
+	};
+
+	// Create the Event Indicators //
+	{
+		var eventIndicators = overlay.selectAll( ".event-indicator" )
+			.data( _events ).enter().append( "g" )
+			.attr( "id", function( _data ) { return _data["name"]; } )
+			.attr( "class", "event-indicator" )
+			.attr( "transform", projectionFunction );
+
+		eventIndicators.append( "circle" )
+			.attr( "cx", 0 ).attr( "cy", 0 )
+			// TODO: Update the radius based on design wants/needs.
+			.attr( "r", 5.0 )
+			.attr( "fill", "red" )
+			.style( "fill-opacity", 1.0 );
+
+		eventIndicators.append( "circle" )
+			.attr( "cx", 0 ).attr( "cy", 0 )
+			// TODO: Update the radius based on design wants/needs.
+			.attr( "r", 10.0 )
+			.attr( "fill", "red" )
+			.style( "fill-opacity", 0.0 );
+	}
+}
+
 function drawMap()
 {
     var width = 960,
@@ -24,7 +69,7 @@ function drawMap()
         
     var map = svg.append("g");
 
-    d3.json('map/nyctopo.json', function(err, json) {
+    d3.json("map/nyctopo.json", function(err, json) {
     //d3.json('map/nyc.json', function(err, json) {
         map.selectAll("path")
             .data(topojson.feature(json, json.objects.nyc).features)
@@ -39,6 +84,19 @@ function drawMap()
             })
             .attr("d", path);
     });
+
+	// TODO: Consider exporting this data to a JSON file instead.
+	var mapEvents = [
+		{
+			"name": "Wall Street Bombing",
+			"date": new Date( 1920, 9, 16, 0, 0, 0, 0 ),
+			"location": "Wall Street, New York",
+			"coordinates": [ -74.008741, 40.706148 ],
+			"sound_radii": [ 5.0, 96.0, 261.0 ]
+		}
+	];
+
+	populateMap( svg, projection, mapEvents );
 }
 
 $(document).ready(function() 
