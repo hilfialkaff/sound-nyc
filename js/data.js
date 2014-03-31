eaa945604fd991feb710ae4d41f6a2cc8d016556
Efe Karakus <efekarakus@gmail.com>
2014-03-30 21:22:37 -0500

    uploading new data points

--- js/data.js
+++ js/data.js
@@ -1,5 +1,37 @@
 var mapEvents = [
     {
+        "name": "Bombing of the Twin Towers",
+        "date": new Date( 2001, 9, 11, 0, 0, 0, 0 ),
+        "location": "Twin Towers, Albany Street, New York, New York",
+        "coordinates": [-74.013436, 40.715087],
+        "sound_radii": [39.0, 200.0, 387.0],
+        "loudness": 198.1
+    },
+    {
+        "name": "Steam Pipe Explosion",
+        "date": new Date(2007, 7, 18, 0, 0, 0, 0),
+        "location": "Lexington Avenue + 41st Street, New York, New York",
+        "coordinates": [-73.972924, 40.755539],
+        "sound_radii": [0.0, 21.0 , 139.0],
+        "loudness": 150.0
+    },
+    {
+        "name": "NYC Ball Drop",
+        "date": new Date(2000, 12, 31, 0, 0, 0, 0),
+        "location": "Times Square, New York, New York",
+        "coordinates": [-73.985455, 40.761651],
+        "sound_radii": [0.0, 0.0, 32.0],
+        "loudness": 120.0
+    },
+    {
+        "name": "Justin Bieber Concert",
+        "date": new Date(2010, 8, 31, 0, 0, 0, 0),
+        "location": "Madison Square Garden, Pennsylvania Plaza, New York, New York",
+        "coordinates": [-73.993352, 40.750598],
+        "sound_radii": [0.0, 0.0, 14],
+        "loudness": 110.0
+    },
+    {
         "name": "Wall Street Bombing",
         "date": new Date( 1920, 9, 16, 0, 0, 0, 0 ),
         "location": "Wall Street, New York",
